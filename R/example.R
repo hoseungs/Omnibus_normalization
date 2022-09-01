774db3f8c1df82c4791f21058fabfeceff9006d9
Hoseung Song <singhosong@gmail.com>
2022-09-01 12:44:19 -0700

    Add files via upload

---
+++ R/example.R
@@ -0,0 +1,18 @@
+# This example refers to the example in an R Package MiRKAT.
+
+data(throat.tree)
+data(throat.otu.tab)
+data(throat.meta)
+
+# X: covariates
+covar = cbind(throat.meta$Age, as.numeric(throat.meta$Sex == "Male"))
+
+n = nrow(throat.meta)
+
+# Y: taxa
+taxa = throat.otu.tab 
+
+# We want to include the first and second covariates in covar: i = c(1,2)
+# The name(s) of clinical variable(s) of interest in covar is the second variable: k = 2
+# We are interested in the sixth taxon in Y: j = 6
+omni(X=covar, Y=taxa, i=c(1,2), j=6, k=2)
